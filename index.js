47fe49a9a59aa1900d649d253c58bb9a1a1089d6
Viktor <velenic@mail.ru>
2022-07-20 18:07:01 +0300

    feat: create textarea element and create KEYBOARD object

---
+++ index.js
@@ -0,0 +1,58 @@
+const body = document.querySelector('body');
+const textAreaEl = document.createElement('textarea');
+body.appendChild(textAreaEl);
+
+const textArea = document.querySelector('textarea');
+textArea.classList.add('text__keyboard');
+textArea.setAttribute('placeholder', 'Enter your text...');
+
+const KEYBOARD = {
+  elements: {
+    info: '',
+    main: null,
+    keysContainer: null,
+    keys: [],
+    layouts: {
+      ru: [
+        'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+        'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\',
+        'caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
+        'lshift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'uarr', 'shift',
+        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr',
+      ],
+      en: [
+        '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+        'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+        'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter',
+        'lshift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'uarr', 'shift',
+        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr',
+      ],
+      ruShifted: [
+        'Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'backspace',
+        'tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/',
+        'caps', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'enter',
+        'lshift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'uarr', 'shift',
+        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr',
+      ],
+      enShifted: [
+        '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'backspace',
+        'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|',
+        'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'enter',
+        'lshift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'uarr', 'shift',
+        'lctrl', 'lang', 'lalt', ' ', 'alt', 'ctrl', 'larr', 'darr', 'rarr',
+      ],
+      whichCodes: [
+        192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
+        9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220,
+        20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13,
+        999, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191,
+      ],
+    },
+  },
+  properties: {
+    value: '',
+    capsLock: null,
+    shift: false,
+    english: null,
+  },
+};
